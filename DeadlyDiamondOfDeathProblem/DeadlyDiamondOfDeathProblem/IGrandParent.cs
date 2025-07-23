35d05be09821909afdc7e314204e73274f7714c9
Rhea-Gomez <rhea.gomez@aurionpro.com>
2025-07-23 10:49:03 +0530

    poc on Deadly Diamond of Death

---
+++ DeadlyDiamondOfDeathProblem/DeadlyDiamondOfDeathProblem/IGrandParent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeadlyDiamondOfDeathProblem
+{
+    public interface IGrandParent
+    {
+        void Display();
+    }
+
+    public interface IParent1 : IGrandParent
+    {
+        void Display();
+    }
+
+    public interface IParent2 : IGrandParent
+    {
+        void Display();
+    }
+
+    public class Child : IParent1, IParent2
+    {
+        void IParent1.Display()
+        {
+            Console.WriteLine("Display from IParent1");
+        }
+
+        void IParent2.Display()
+        {
+            Console.WriteLine("Display from IParent2");
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Display from Child");
+        }
+    }
+}
---
+++ DeadlyDiamondOfDeathProblem/DeadlyDiamondOfDeathProblem/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DeadlyDiamondOfDeathProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Child obj = new Child();
+            obj.Display();                      //calls Child's display
+
+            //Explicit calls
+            ((IParent1)obj).Display();          //calls IParent1's Display
+            ((IParent2)obj).Display();          //calls IParent2's Display
+        }
+    }
+}
